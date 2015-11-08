c67e1de827d2f06fafd8aa8984381cbdfde27505
cueBall86 <anupmohan2@gmail.com>
2015-11-08 01:48:35 -0600

    Fixed MAP API

--- app/src/main/java/pebble/cue/trafficalarm/MainActivity.java
+++ app/src/main/java/pebble/cue/trafficalarm/MainActivity.java
@@ -17,15 +17,21 @@ import com.getpebble.android.kit.Constants;
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapFragment;
+
 public class MainActivity extends ActionBarActivity {
+    // map embedded in the map fragment
+    private Map map = null;
+
+    // map fragment embedded in this activity
+    private MapFragment mapFragment = null;
     private PebbleKit.PebbleDataReceiver mReceiver;
     private Handler mHandler = new Handler();
     private final static UUID COMPESS_UUID = UUID.fromString("2d0d78a2-6af6-4890-a1b4-0f568611383f");
@@ -36,6 +42,31 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button launchButton = (Button)findViewById(R.id.launch_button);
+        // Search for the map fragment to finish setup by calling init().
+        mapFragment = (MapFragment)getFragmentManager().findFragmentById(
+                R.id.mapfragment);
+        mapFragment.init(new OnEngineInitListener() {
+            @Override
+            public void onEngineInitializationCompleted(
+                    OnEngineInitListener.Error error)
+            {
+                if (error == OnEngineInitListener.Error.NONE) {
+                    // retrieve a reference of the map from the map fragment
+                    map = mapFragment.getMap();
+                    // Set the map center to the Vancouver region (no animation)
+                    map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0),
+                            Map.Animation.NONE);
+                    // Set the zoom level to the average between min and max
+                    map.setZoomLevel(
+                            (map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
+                } else {
+                    System.out.println("ERROR: Cannot initialize Map Fragment" + error.toString());
+
+                }
+            }
+        });
+
+
         launchButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
